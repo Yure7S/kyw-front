2acb3f38896f76eb77c7784c9edf718059b33bb7
Yure7S <yuresilvaprof@gmail.com>
2024-05-04 13:26:34 -0300

    feat: Instalando a lib ngx-mask

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainInputComponent } from '../../shared/components/main-input/main-input.component';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { ErrorSpanComponent } from '../../shared/components/error-span/error-span.component';
+import { IdValuePipe } from '../../shared/pipes/id-value.pipe';
+import { ValidationErrorPipe } from '../../shared/pipes/validation-error.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainInputComponent,
+    ErrorSpanComponent,
+    IdValuePipe,
+    ValidationErrorPipe
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+  ],
+  exports: [
+    MainInputComponent
+  ],
+  providers: [
+    provideNgxMask(),
   ]
 })
 export class SharedModule { }
