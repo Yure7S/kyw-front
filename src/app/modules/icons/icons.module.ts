5d320826672e1e53a5eeaa3fb734a6f64ef95023
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 12:12:44 -0300

    feat: Criando o IconsModule

---
+++ src/app/modules/icons/icons.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFile, far } from '@fortawesome/free-regular-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [FontAwesomeModule]
+})
+export class IconsModule { 
+
+  constructor(library: FaIconLibrary) {
+
+    library.addIconPacks(far)
+
+    library.addIcons(
+      faArrowLeft
+    )
+  }
+}
