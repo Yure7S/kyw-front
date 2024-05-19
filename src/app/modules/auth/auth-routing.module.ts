1746b7414774cf247c5b4d0c21df74467764fcb8
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 13:38:45 -0300

    feat: Configurando as rotas para a página de cadastro

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: RegisterComponent }
 ];
 
 @NgModule({
