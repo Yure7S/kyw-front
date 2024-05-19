61e7fd4b9bbf0b2f6167a7b35ba7b737625097f8
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 13:38:25 -0300

    feat: Criando a página RegisterComponent

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginTemplateComponent } from './components/templates/login-template/login-template.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     LoginTemplateComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
