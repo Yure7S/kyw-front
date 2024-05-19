a028269e756e40943223f642efd945f4ab06f5a1
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 09:50:02 -0300

    feat: Criando estrutura base para o CoreModule

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
