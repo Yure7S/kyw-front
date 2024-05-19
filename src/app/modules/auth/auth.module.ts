70eefd11f97ded2eb405d8e8c4dee3e915e9cc2a
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 12:21:36 -0300

    feat: Criando configuração básica de rotas para a página de login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginTemplateComponent } from './components/templates/login-template/login-template.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginTemplateComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
