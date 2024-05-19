fed96939e1cebaaefc28bd81e0f2e4c99e7c3097
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 09:49:48 -0300

    feat: Criando estrutura base da página HomeComponent

---
+++ src/app/modules/core/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+}
