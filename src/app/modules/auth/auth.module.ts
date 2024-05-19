ffa6c52dc0d10f94dabd122598c0529f02bf1e51
Yure7S <yuresilvaprof@gmail.com>
2024-05-19 13:43:47 -0300

    refactor: Renomeando o componente LoginTemplateComponent por AuthTemplateComponent

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginTemplateComponent } from './components/templates/login-template/login-template.component';
+import { AuthTemplateComponent } from './components/templates/auth-template/auth-template.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterComponent } from './pages/register/register.component';
@@ -10,7 +10,7 @@ import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    LoginTemplateComponent,
+    AuthTemplateComponent,
     LoginComponent,
     RegisterComponent
   ],
