6187eeff7cba4a199fe11804ad5bfa506e7627e5
Yure7S <yuresilvaprof@gmail.com>
2024-06-04 18:57:58 -0300

    atualizando

--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)},
+  {path: '', component: HomeComponent},
+  {path: 'auth', loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)},
   {path: 'criar-projeto', loadChildren: () => import('../create-project/create-project.module').then(m => m.CreateProjectModule)}
 ];
 
