9a8fde826ab4f448fed2cc27ebe133790472a29f
Yure7S <yuresilvaprof@gmail.com>
2024-06-03 23:01:48 -0300

    feat: Finalizando o componente ProfileImageInputComponent

--- src/app/shared/components/profile-image-input/profile-image-input.component.ts
+++ src/app/shared/components/profile-image-input/profile-image-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-profile-image-input',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-image-input.component.scss']
 })
 export class ProfileImageInputComponent {
+  @Output() removeFile: EventEmitter<any> = new EventEmitter()
+  @Output() addFile: EventEmitter<File> = new EventEmitter()
+  @Input({ required: true }) idValue!: string
+  @Input() disabled: boolean = false
+  @Input() minHeight: string = ''
+  @Input() currentFile?: File
 
+  onFileDropped(event: any) {
+    const file: File = event instanceof DragEvent
+      ? event.dataTransfer?.files[0]!
+      : event.target?.files[0]
+    this.currentFile = file
+    this.addFile.emit(this.currentFile)
+  }
+
+  onRemoveFile() {
+    this.currentFile = undefined
+    this.removeFile.emit()
+  }
 }
