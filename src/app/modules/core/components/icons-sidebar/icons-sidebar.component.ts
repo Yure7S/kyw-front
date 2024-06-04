0f20385d430bebb8c0916d39d6ebe7c9d6e24dfa
Yure7S <yuresilvaprof@gmail.com>
2024-06-04 18:57:00 -0300

    feat: Criando o componente IconsSidebarComponent

---
+++ src/app/modules/core/components/icons-sidebar/icons-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icons-sidebar',
+  templateUrl: './icons-sidebar.component.html',
+  styleUrls: ['./icons-sidebar.component.scss']
+})
+export class IconsSidebarComponent {
+
+}
