185775a8b23cf2d74b8449403dc738ad09b37f54
Yure7S <yuresilvaprof@gmail.com>
2024-06-03 23:35:19 -0300

    feat: Configurando a versão noPadding do SingleContainerComponent

--- src/app/shared/components/single-container/single-container.component.ts
+++ src/app/shared/components/single-container/single-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-container',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-container.component.scss']
 })
 export class SingleContainerComponent {
-
+ @Input() noPadding: boolean = false
 }
