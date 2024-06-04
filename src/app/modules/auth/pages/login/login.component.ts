3527d106dfb579deb98c5dc15471b7eb99a787f1
Yure7S <yuresilvaprof@gmail.com>
2024-06-03 22:46:01 -0300

    feat: Implementando o formulário de login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Login } from 'src/app/core/model/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,21 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  tempControl = new FormControl()
+  formBuilder = inject(FormBuilder)
+  toastService = inject(ToastrService)
+
+  form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  })
+
+  onSubmit(): void {
+    if(this.form.invalid) {
+      this.toastService.error('Dados inválidos', 'Erros')
+      return
+    } 
+    
+    const loginData = <Login>this.form.value
+    console.log(loginData)
+  }
 }
