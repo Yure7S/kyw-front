5fb767c67ba4fcea180fa0491e80fe0ec6c3fe75
Yure7S <yuresilvaprof@gmail.com>
2024-06-03 23:40:07 -0300

    feat: Criando métodos para manipular a lista de emails

--- src/app/modules/create-project/pages/members/members.component.ts
+++ src/app/modules/create-project/pages/members/members.component.ts
@@ -10,4 +10,18 @@ import { Status } from 'src/app/core/enums/status.enum';
 export class MembersComponent {
   temp = new FormControl()
   statusEnum: typeof Status = Status
+
+  memberEmailList: string[] = [
+    'teste@gmail.com',
+    'outroemail@gmail.com',
+    'joaoteste@gmail.com'
+  ]
+
+  removeEmail(email: string): void {
+    this.memberEmailList = this.memberEmailList.filter(item => item != email)
+  } 
+  
+  addEmail(email: string): void {
+    this.memberEmailList.push(email)
+  } 
 }
