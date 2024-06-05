0948c3ae0a2569a4cc4894b57121d967fa81a254
Yure7S <yuresilvaprof@gmail.com>
2024-06-05 17:41:56 -0300

    feat: Configurando rotas do NotificationModule

--- src/app/modules/notification/notification-routing.module.ts
+++ src/app/modules/notification/notification-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NotificationsComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/notification/notification.module.ts
+++ src/app/modules/notification/notification.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotificationRoutingModule } from './notification-routing.module';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotificationsComponent
+  ],
   imports: [
     CommonModule,
-    NotificationRoutingModule
+    NotificationRoutingModule,
+    SharedModule
   ]
 })
 export class NotificationModule { }
