0c369ea0931d9274fb912a5b99ca66cc1cbaa826
Yure7S <yuresilvaprof@gmail.com>
2024-06-05 17:40:03 -0300

    feat: Configurando rotas do TaskRoutingModule

--- src/app/modules/task/task-routing.module.ts
+++ src/app/modules/task/task-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: TasksComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TaskRoutingModule } from './task-routing.module';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TasksComponent
+  ],
   imports: [
     CommonModule,
-    TaskRoutingModule
+    TaskRoutingModule,
+    SharedModule
   ]
 })
 export class TaskModule { }
