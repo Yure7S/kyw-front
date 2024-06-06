c4f54a229fdca8035dd716081ad5fd637be07ace
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 09:08:54 -0300

    feat: Configurando o authGuardGuard

--- src/app/core/guard/route-guard/auth-guard.guard.ts
+++ src/app/core/guard/route-guard/auth-guard.guard.ts
@@ -1,6 +1,9 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { CurrentUserService } from '../../utils/current-user.service';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
-  console.log('***** authGuardGuard ativo *****')
-  return true;
+
+  const currentUserService = inject(CurrentUserService)
+  return currentUserService.onlyLoggedInd('Você precisa estar logado para acessar esta página');
 };
