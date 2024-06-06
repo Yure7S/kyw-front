427d431c8d2368d258c49c1cb34ba5e5f994b610
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 16:39:54 -0300

    feat: Criando o método itemSideBarTrack no trackbyservice

--- src/app/core/utils/track-by.service.ts
+++ src/app/core/utils/track-by.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, TrackByFunction } from '@angular/core';
+import { Sidebar } from '../model/sidebar.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,9 @@ export class TrackByService {
 
   public track(index: number, item: any): TrackByFunction<any> {
     return item.id
+  }
+
+  public itemSideBarTrack(): TrackByFunction<Sidebar> {
+    return (index: number, item: Sidebar) => item.content
   }
 }
