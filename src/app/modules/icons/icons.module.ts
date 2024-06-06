8613a41094043614d5ef9f66a7e0a715b2965616
Yure7S <yuresilvaprof@gmail.com>
2024-06-05 23:10:04 -0300

    feat: Corrigindo o ícone do SearchInputComponent

--- src/app/modules/icons/icons.module.ts
+++ src/app/modules/icons/icons.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBell, faChartBar, faFile, faTrashCan, far } from '@fortawesome/free-regular-svg-icons';
-import { faArrowLeft, faArrowRight, faChevronLeft, faEllipsisVertical, faGear, faListCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faChevronLeft, faEllipsisVertical, faGear, faInfoCircle, faListCheck, faMagnifyingGlass, faPlus, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [],
@@ -28,6 +28,9 @@ export class IconsModule {
       faListCheck,
       faChartBar,
       faChevronLeft,
+      faInfoCircle,
+      faTriangleExclamation,
+      faMagnifyingGlass,
     )
   }
 }
