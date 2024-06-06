cd0d6ebff2ab8b34ad47b9cee480e1ec69af5696
Yure7S <yuresilvaprof@gmail.com>
2024-06-05 23:15:46 -0300

    feat: Implementando o formulário de login e armazenando a sessão do usuário

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,7 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Status } from 'src/app/core/enums/status.enum';
+import { ErrorResponse } from 'src/app/core/model/error-response.interface';
+import { LoginResponse } from 'src/app/core/model/login-response.interface';
 import { Login } from 'src/app/core/model/login.interface';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { validationErrorMessages } from 'src/app/shared/error-message-validators/validation-error-messages';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +18,12 @@ import { Login } from 'src/app/core/model/login.interface';
 export class LoginComponent {
   formBuilder = inject(FormBuilder)
   toastService = inject(ToastrService)
+  authService = inject(AuthService)
+  router = inject(Router)
+
+  response?: ErrorResponse
+  statusEnum: typeof Status = Status
+  errorMessages = validationErrorMessages
 
   form = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -18,12 +31,17 @@ export class LoginComponent {
   })
 
   onSubmit(): void {
-    if(this.form.invalid) {
-      this.toastService.error('Dados inválidos', 'Erros')
-      return
-    } 
-    
-    const loginData = <Login>this.form.value
-    console.log(loginData)
+    if (this.form.valid) {
+      const loginData = <Login>this.form.value
+      this.authService.login(loginData).subscribe({
+        next: (r: LoginResponse) => {
+          localStorage.clear()
+          localStorage.setItem('session', JSON.stringify(r))
+          this.toastService.success('Login realizado com sucesso!', 'Sucesso')
+          this.router.navigateByUrl('')
+        },
+        error: (error: ErrorResponse) => this.response = error,
+      })
+    } else this.toastService.warning('Dados inválidos!', 'Alerta')
   }
 }
