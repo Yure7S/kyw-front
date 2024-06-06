d20863b493fb71deb0e5020fa8ee6a9d01a06fb4
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 15:52:49 -0300

    feat: Atualizando alguns estilos

--- src/app/modules/project/project-routing.module.ts
+++ src/app/modules/project/project-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { SidebarComponent } from 'src/app/shared/components/sidebars/sidebar/sidebar.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'projetos'},
   {
     path: 'projetos', component: ProjectsComponent,
     children: [
-      {path: ':projectId', component: SidebarComponent}
+      {path: ':projectId', component: ProjectDetailsComponent}
     ]
   }
 ];
--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -5,12 +5,16 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { SharedModule } from '../shared/shared.module';
 import { SidebarProjectCardComponent } from './components/sidebar-project-card/sidebar-project-card.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
+import { ProjectImageComponent } from './components/project-image/project-image.component';
 
 
 @NgModule({
   declarations: [
     ProjectsComponent,
     SidebarProjectCardComponent,
+    ProjectDetailsComponent,
+    ProjectImageComponent,
   ],
   imports: [
     CommonModule,
