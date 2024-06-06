308b04312a14c83595787baf090db4a4582f56f9
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 10:49:46 -0300

    feat: Exibindo a lista de projetos

--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { SharedModule } from '../shared/shared.module';
+import { SidebarProjectCardComponent } from './components/sidebar-project-card/sidebar-project-card.component';
 
 
 @NgModule({
   declarations: [
     ProjectsComponent,
+    SidebarProjectCardComponent,
   ],
   imports: [
     CommonModule,
