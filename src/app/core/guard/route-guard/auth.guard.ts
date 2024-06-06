f77b23cfa7a122231cea414d4b0a73da6f153b3a
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 09:27:01 -0300

    feat: Renomeando o authGuard

--- src/app/core/guard/route-guard/auth.guard.ts
+++ src/app/core/guard/route-guard/auth.guard.ts
@@ -2,8 +2,8 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { CurrentUserService } from '../../utils/current-user.service';
 
-export const authGuardGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route, state) => {
 
   const currentUserService = inject(CurrentUserService)
-  return currentUserService.onlyLoggedInd('Você precisa estar logado para acessar esta página');
+  return currentUserService.onlyLoggedInd();
 };
