9b7709fe238755dd44246672911c4ffed0e4e947
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 09:57:48 -0300

    feat: Configurndo a versão smaller do AddButtonComponent

--- src/app/shared/components/buttons/add-button/add-button.component.ts
+++ src/app/shared/components/buttons/add-button/add-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-button',
@@ -7,4 +7,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class AddButtonComponent {
   @Output() onClick: EventEmitter<any> = new EventEmitter()
+  @Input() smaller: boolean = false
+  @Input() titleValue: string = ''
+  @Input() routerLinkValue: string = ''
 }
