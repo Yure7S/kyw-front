75278a462e8663bc0be6c806cd81ad77865a80dc
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 09:26:23 -0300

    feat: Ajustando as rotas do AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guard/route-guard/auth.guard';
 
 const routes: Routes = [
-  { path: "", loadChildren: () => import('./modules/core/core.module').then(m => m.CoreModule) }
+  { path: '', canActivate: [authGuard], loadChildren: () => import('./modules/core/core.module').then(m => m.CoreModule) },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
