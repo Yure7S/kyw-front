f8da514178b243a4007f7c728997165814269c19
Yure7S <yuresilvaprof@gmail.com>
2024-06-06 10:49:16 -0300

    feat: Criando o mock da projectList

--- src/app/modules/project/pages/projects/projects.component.ts
+++ src/app/modules/project/pages/projects/projects.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Project } from 'src/app/core/model/project.interface';
+import { TrackByService } from 'src/app/core/utils/track-by.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  trackByService = inject(TrackByService)
+
+  projectList: Project[] = [
+    {
+      id: 'asdfasdf',
+      name: 'Nome do projeto',
+      description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.',
+      creator: {
+        userId: '',
+        nickname: ''
+      },
+      imageUrl: '',
+      linkGroup: '',
+      pin: false,
+      createAt: '2024-06-06T13:10:42.942Z'
+    },
+    {
+      id: 'asdfasdf',
+      name: 'Nome do projeto',
+      description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.',
+      creator: {
+        userId: '',
+        nickname: ''
+      },
+      imageUrl: '',
+      linkGroup: '',
+      pin: false,
+      createAt: '2024-06-06T13:10:42.942Z'
+    },
+    {
+      id: 'asdfasdf',
+      name: 'Nome do projeto',
+      description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.',
+      creator: {
+        userId: '',
+        nickname: ''
+      },
+      imageUrl: '',
+      linkGroup: '',
+      pin: false,
+      createAt: '2024-06-06T13:10:42.942Z'
+    },
+  ]
 }
