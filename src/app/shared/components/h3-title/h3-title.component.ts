fbdda04b1b368b0c4669b1edf74d30d910cb9b79
Yure7S <yuresilvaprof@gmail.com>
2024-06-07 15:21:52 -0300

    feat: Criando o componente H3TitleComponent

---
+++ src/app/shared/components/h3-title/h3-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-h3-title',
+  templateUrl: './h3-title.component.html',
+  styleUrls: ['./h3-title.component.scss']
+})
+export class H3TitleComponent {
+
+}
