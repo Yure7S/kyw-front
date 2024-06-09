f6a548f314d7349702db8ab03b9fb4a72865204d
Yure7S <yuresilvaprof@gmail.com>
2024-06-09 13:56:01 -0300

    feat: Criando o componente ProfileImageComponent

---
+++ src/app/shared/components/profile-image/profile-image.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-image',
+  templateUrl: './profile-image.component.html',
+  styleUrls: ['./profile-image.component.scss']
+})
+export class ProfileImageComponent {
+  @Input() src?: string
+  @Input() smaller: boolean = false
+}
