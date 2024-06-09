3f9581167226d57ddabe94a8ae4dbce009f59316
Yure7S <yuresilvaprof@gmail.com>
2024-06-09 13:58:28 -0300

    remove: Deletando o componente ProjectImageComponent

--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -6,7 +6,6 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import { SharedModule } from '../shared/shared.module';
 import { SidebarProjectCardComponent } from './components/sidebar-project-card/sidebar-project-card.component';
 import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
-import { ProjectImageComponent } from './components/project-image/project-image.component';
 
 
 @NgModule({
@@ -14,7 +13,6 @@ import { ProjectImageComponent } from './components/project-image/project-image.
     ProjectsComponent,
     SidebarProjectCardComponent,
     ProjectDetailsComponent,
-    ProjectImageComponent,
   ],
   imports: [
     CommonModule,
