be8d3af89cd74995f68deb8d49452c994ed10334
Yure7S <yuresilvaprof@gmail.com>
2024-06-09 18:27:13 -0300

    feat: Configurando a versão do loader no ButtonMainActionComponent

--- src/app/shared/components/buttons/button-main-action/button-main-action.component.ts
+++ src/app/shared/components/buttons/button-main-action/button-main-action.component.ts
@@ -7,5 +7,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ButtonMainActionComponent {
  @Input() fullWidth: boolean = false
+ @Input() loading: boolean = false
  @Output() onCLick: EventEmitter<any> = new EventEmitter()
 }
