751c0f2cc9e7854eed72c864bf69b0f1da73fc14
Yure7S <yuresilvaprof@gmail.com>
2024-06-09 17:51:05 -0300

    feat: Implementando o formulário da página de cadastro

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,5 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Status } from 'src/app/core/enums/status.enum';
+import { CurrentUser } from 'src/app/core/model/current-user.interface';
+import { ErrorResponse } from 'src/app/core/model/error-response.interface';
+import { Login } from 'src/app/core/model/login.interface';
+import { Register } from 'src/app/core/model/register.interface';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { CurrentUserService } from 'src/app/core/utils/current-user.service';
+import { validationErrorMessages } from 'src/app/shared/error-message-validators/validation-error-messages';
+import { CustomValidators } from 'src/app/shared/validators/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +20,35 @@ import { FormBuilder, FormControl } from '@angular/forms';
 export class RegisterComponent {
 
   formBuilder = inject(FormBuilder)
+  toastService = inject(ToastrService)
+  authService = inject(AuthService)
+  router = inject(Router)
 
-  registerType: FormControl = new FormControl(true)
+  registerType: FormControl = new FormControl()
+  response?: ErrorResponse
+  statusEnum: typeof Status = Status
+  errorMessages = validationErrorMessages
 
   form = this.formBuilder.group({
-    tempControl: [''],
+    nickname: ['', [Validators.required, Validators.maxLength(128)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(128)]],
+    phone: ['', [Validators.required, Validators.maxLength(11), Validators.minLength(8)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', [Validators.required, CustomValidators.identicalPassword('password')]],
   })
+
+  onSubmit(): void {
+    console.log(this.form)
+    if (this.form.valid) {
+      const {confirmPassword, ...formValue} = this.form.value
+      const registerData = <Register>formValue
+      this.authService.register(registerData).subscribe({
+        next: (r: CurrentUser) => {
+          this.toastService.success('Cadastro realizado com sucesso!', 'Sucesso')
+          this.router.navigateByUrl('auth/login')
+        },
+        error: (error: ErrorResponse) => this.response = error,
+      })
+    } else this.toastService.warning('Dados inválidos!', 'Alerta')
+  }
 }
