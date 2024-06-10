e16164ccfb92614779409e3847575fb1d0fa86ea
Yure7S <yuresilvaprof@gmail.com>
2024-06-10 17:56:45 -0300

    refactor: Atualizando o tipo do projectList$

--- src/app/modules/project/pages/projects/projects.component.ts
+++ src/app/modules/project/pages/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, take, tap } from 'rxjs';
+import { Observable, map, take, tap } from 'rxjs';
 import { GetterResponse } from 'src/app/core/model/getter-response.interface';
 import { Project } from 'src/app/core/model/project.interface';
 import { ProjectService } from 'src/app/core/service/project.service';
@@ -16,7 +16,7 @@ export class ProjectsComponent implements OnInit {
   trackByService = inject(TrackByService)
   projectService = inject(ProjectService)
 
-  projectList$?: Observable<GetterResponse<Project[]>>
+  projectList$?: Observable<any>
 
   ngOnInit(): void {
     this.projectList$ = this.projectService.getAll()
