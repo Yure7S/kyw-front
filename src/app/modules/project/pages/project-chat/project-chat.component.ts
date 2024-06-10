e3ef5cfa0f8756e5656796d28bbad529f31e4f30
Yure7S <yuresilvaprof@gmail.com>
2024-06-09 23:03:04 -0300

    feat: Iniciando o densenvolvimento da página ProjectChatComponent

---
+++ src/app/modules/project/pages/project-chat/project-chat.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ChatService } from 'src/app/core/utils/chat.service';
+
+@Component({
+  selector: 'app-project-chat',
+  templateUrl: './project-chat.component.html',
+  styleUrls: ['./project-chat.component.scss']
+})
+export class ProjectChatComponent implements OnInit, OnDestroy {
+  chatService = inject(ChatService)
+
+  messageControl: FormControl = new FormControl()
+
+
+  ngOnInit(): void {
+    this.chatService.openConnection()
+  }
+
+  ngOnDestroy(): void {
+    this.chatService.closeConnection()
+  }
+
+  send() {
+    this.chatService.sendMessage(this.messageControl.value)
+    this.messageControl.reset()
+  }
+}
