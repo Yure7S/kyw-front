3f0153f2bcf81e05d8453d41e6350aba3629378e
Yure7S <yuresilvaprof@gmail.com>
2024-06-10 11:01:32 -0300

    feat: Adicionando o status a SidebarTaskCardComponent

--- src/app/modules/task/pages/tasks/tasks.component.ts
+++ src/app/modules/task/pages/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { TaskStatus } from 'src/app/core/enums/TaskStatus.enum';
 import { Task } from 'src/app/core/model/task.interface';
 import { TrackByService } from 'src/app/core/utils/track-by.service';
 
@@ -34,7 +35,7 @@ export class TasksComponent {
     {
       id: 'asdfadsfsf',
       title: 'É um facto',
-      status: 'open',
+      status: TaskStatus.Pending,
       criticality: 'critical',
       description: 'É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por "lorem ipsum" irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor).',
       attachments: '',
