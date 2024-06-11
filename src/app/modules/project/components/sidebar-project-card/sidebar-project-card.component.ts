8a8bdb04eb134c9764d4d41b520f9e98d3a47b15
Yure7S <yuresilvaprof@gmail.com>
2024-06-11 10:57:18 -0300

    feat: Recebendo a url no SidebarProjectCardComponent

--- src/app/modules/project/components/sidebar-project-card/sidebar-project-card.component.ts
+++ src/app/modules/project/components/sidebar-project-card/sidebar-project-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Project } from 'src/app/core/model/project.interface';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-sidebar-project-card',
@@ -8,4 +9,5 @@ import { Project } from 'src/app/core/model/project.interface';
 })
 export class SidebarProjectCardComponent {
   @Input() project!: Project
+  @Input() url!: string
 }
