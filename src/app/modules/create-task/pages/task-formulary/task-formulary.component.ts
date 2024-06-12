1b472ffbab7fa75bee70faf48f0929780c592e85
Yure7S <yuresilvaprof@gmail.com>
2024-06-12 18:25:41 -0300

    feat: Configurando o formulário da página TaskFormularyComponent

--- src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
+++ src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Criticality } from 'src/app/core/enums/criticality.enum';
 import { Status } from 'src/app/core/enums/status.enum';
+import { TaskService } from 'src/app/core/service/task.service';
 import { validationErrorMessages } from 'src/app/shared/error-message-validators/validation-error-messages';
 
 @Component({
@@ -10,13 +13,18 @@ import { validationErrorMessages } from 'src/app/shared/error-message-validators
 })
 export class TaskFormularyComponent {
 
+  taskService = inject(TaskService)
+  toastService = inject(ToastrService)
   formBuilder = inject(FormBuilder)
+  
   errorMessages = validationErrorMessages
   statusEnum: typeof Status = Status 
 
   form = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.maxLength(128)]],
-    description: ['', [Validators.required, Validators.maxLength(258)]],
+    title: ['', [Validators.required, Validators.maxLength(128)]],
+    criticality: [Criticality, [Validators.required]],
+    deadline: ['', [Validators.required]],
+    description: ['', [Validators.required, Validators.maxLength(256)]],
   })
 
   onSubmit() {
