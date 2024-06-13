5c44344ad6399d4780a40ffc33ec6468afa2cbe3
Yure7S <yuresilvaprof@gmail.com>
2024-06-13 11:03:07 -0300

    feat: Implementando a lista de anexos no TaskFormularyComponent

--- src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
+++ src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Criticality } from 'src/app/core/enums/criticality.enum';
 import { Status } from 'src/app/core/enums/status.enum';
 import { TaskService } from 'src/app/core/service/task.service';
+import { TrackByService } from 'src/app/core/utils/track-by.service';
 import { validationErrorMessages } from 'src/app/shared/error-message-validators/validation-error-messages';
 
 @Component({
@@ -15,10 +16,13 @@ export class TaskFormularyComponent {
 
   taskService = inject(TaskService)
   toastService = inject(ToastrService)
+  trackByService = inject(TrackByService)
   formBuilder = inject(FormBuilder)
+  renderer = inject(Renderer2)
 
   errorMessages = validationErrorMessages
-  statusEnum: typeof Status = Status 
+  statusEnum: typeof Status = Status
+  fileList: File[] = []
 
   form = this.formBuilder.group({
     title: ['', [Validators.required, Validators.maxLength(128)]],
@@ -26,6 +30,26 @@ export class TaskFormularyComponent {
     deadline: ['', [Validators.required]],
     description: ['', [Validators.required, Validators.maxLength(256)]],
   })
+
+  addFile(file: File) {
+    if (this.fileList.includes(file)) {
+      this.toastService.warning('Você já adicionou este arquivo!', 'Alerta')
+    } else this.fileList.push(file)
+  }
+
+  removeFile(file: File) {
+    this.fileList = this.fileList.filter(item => item !== file)
+  }
+
+  downloadFile(file: File) {
+    const fileUrl = window.URL.createObjectURL(file)
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', fileUrl);
+    link.setAttribute('download', file.name);
+    link.click();
+    link.remove();
+  }
 
   onSubmit() {
 
