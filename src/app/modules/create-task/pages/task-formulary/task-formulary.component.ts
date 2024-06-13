d500761a1041119a764690c6ff836bd2071f32bd
Yure7S <yuresilvaprof@gmail.com>
2024-06-13 17:50:00 -0300

    feat: Implementando a lógica para a lista de membros

--- src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
+++ src/app/modules/create-task/pages/task-formulary/task-formulary.component.ts
@@ -1,18 +1,23 @@
-import { Component, Renderer2, inject } from '@angular/core';
+import { Component, OnInit, Renderer2, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { switchMap } from 'rxjs';
 import { Criticality } from 'src/app/core/enums/criticality.enum';
 import { Status } from 'src/app/core/enums/status.enum';
+import { Member } from 'src/app/core/model/member.interface';
 import { ProjectService } from 'src/app/core/service/project.service';
 import { TaskService } from 'src/app/core/service/task.service';
+import { ModalService } from 'src/app/core/utils/modal.service';
 import { TrackByService } from 'src/app/core/utils/track-by.service';
+import { MemberModalComponent } from 'src/app/shared/components/member-modal/member-modal.component';
 import { validationErrorMessages } from 'src/app/shared/error-message-validators/validation-error-messages';
 
 @Component({
   selector: 'app-task-formulary',
   templateUrl: './task-formulary.component.html',
-  styleUrls: ['./task-formulary.component.scss']
+  styleUrls: ['./task-formulary.component.scss'],
+  providers: [ModalService]
 })
 export class TaskFormularyComponent {
 
@@ -21,6 +26,7 @@ export class TaskFormularyComponent {
   trackByService = inject(TrackByService)
   projectService = inject(ProjectService)
   activatedRoute = inject(ActivatedRoute)
+  modalService = inject(ModalService)
   formBuilder = inject(FormBuilder)
   renderer = inject(Renderer2)
 
@@ -28,6 +34,7 @@ export class TaskFormularyComponent {
   statusEnum: typeof Status = Status
   projectId: string = this.activatedRoute.snapshot.paramMap.get('projectId') ?? ''
   fileList: File[] = []
+  memberList: Member[] = []
 
   form = this.formBuilder.group({
     title: ['', [Validators.required, Validators.maxLength(128)]],
@@ -37,9 +44,21 @@ export class TaskFormularyComponent {
   })
 
   addMembers() {
-    this.projectService.getMembers(this.projectId).subscribe(console.log)
+    this.projectService.getMembers(this.projectId).pipe(
+      switchMap(memberList => {
+        return this.modalService.open({
+          title: 'Membros',
+          memberList: memberList,
+          description: 'Selecione os membros que serão responsáveis por esta tarefa.'
+        }, MemberModalComponent)
+      })
+    ).subscribe(selectedMembers => this.memberList = selectedMembers)
   }
-  
+
+  removeMember(deletedMember: Member) {
+    this.memberList = this.memberList.filter(member => member !== deletedMember)
+  }
+
   addFile(file: File) {
     if (this.fileList.includes(file)) {
       this.toastService.warning('Você já adicionou este arquivo!', 'Alerta')
