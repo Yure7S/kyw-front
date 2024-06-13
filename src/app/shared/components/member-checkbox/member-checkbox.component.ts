14698aa25aca009767109a4007bfda1bbd90aa1b
Yure7S <yuresilvaprof@gmail.com>
2024-06-13 17:19:55 -0300

    feat: Refatorando o MemberCheckboxComponent

--- src/app/shared/components/member-checkbox/member-checkbox.component.ts
+++ src/app/shared/components/member-checkbox/member-checkbox.component.ts
@@ -5,42 +5,12 @@ import { Member } from 'src/app/core/model/member.interface';
 @Component({
   selector: 'app-member-checkbox',
   templateUrl: './member-checkbox.component.html',
-  styleUrls: ['./member-checkbox.component.scss'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => MemberCheckboxComponent),
-    multi: true
-  }]
+  styleUrls: ['./member-checkbox.component.scss']
 })
-export class MemberCheckboxComponent implements ControlValueAccessor {
-  @Output() onBlur: EventEmitter<any> = new EventEmitter()
-  // @Output() toggle: EventEmitter<boolean> = new EventEmitter()
+export class MemberCheckboxComponent {
+  @Output() toggle: EventEmitter<boolean> = new EventEmitter()
   @Input() idValue?: string
   @Input() member?: Member
-  @Input() disabled: boolean = false
   @Input() checked: boolean = false
   @Input() value?: boolean
-
-  changeDetector = inject(ChangeDetectorRef)
-
-  private onChanged = (value: boolean) => { }
-  public onTouched = (v: boolean) => { }
-
-  writeValue(value: boolean): void {
-    this.onChanged(value)
-    this.value = value
-    this.changeDetector.detectChanges()
-  }
-
-  registerOnChange(fn: (value: boolean) => {}): void {
-    this.onChanged = fn
-  }
-
-  registerOnTouched(fn: () => {}): void {
-    this.onTouched = fn
-  }
-
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled
-  }
 }
