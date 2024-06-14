f59fdf377f31e021a323662f0dc6bea8fae0aff7
Yure7S <yuresilvaprof@gmail.com>
2024-06-14 18:47:36 -0300

    feat: Configurando o SocketIoModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { ToastrModule } from 'ngx-toastr';
 import { ErrorHandlingInterceptor } from './core/guard/interceptor/error-handling.interceptor';
 import { CurrentUserService } from './core/utils/current-user.service';
 import { RefreshTokenInterceptor } from './core/guard/interceptor/refresh-token.interceptor';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+  url: 'ws://localhost:8080',
+  options: {
+    path: '/connect',
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJici5jb20ua3l3Iiwic3ViIjoic2VuaGFAZ21haWwuY29tIiwiaWQiOiJjYWVkNjE1Yy04ZjdhLTRlNzktYTk3My1jOTU2MTE3NzNmNzIiLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiZXhwIjoxNzE4NDAyNTIxfQ.4QfC39gnf3ptYsH81ePhMG_YISYIdXmWJmiTivMHP10"
+        }
+      }
+    }
+  }
+};
+
 
 @NgModule({
   declarations: [
@@ -21,6 +37,7 @@ import { RefreshTokenInterceptor } from './core/guard/interceptor/refresh-token.
     AppRoutingModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
+    SocketIoModule.forRoot(config),
     ToastrModule.forRoot({
       progressBar: true,
       toastClass: 'toast ngx-toastr',
