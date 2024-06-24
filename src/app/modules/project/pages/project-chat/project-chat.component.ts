ca7d402f633dac15bf6e47aa85cffd9cea04d4a9
Yure7S <yuresilvaprof@gmail.com>
2024-06-24 17:57:34 -0300

    feat: Implementando a lógica do chat na página  ProjectChatComponent

--- src/app/modules/project/pages/project-chat/project-chat.component.ts
+++ src/app/modules/project/pages/project-chat/project-chat.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable, of, take } from 'rxjs';
+import { MessageResponse } from 'src/app/core/model/message-response.model';
 import { Message } from 'src/app/core/model/message.model';
 import { MessageService } from 'src/app/core/service/message.service';
 import { ProjectService } from 'src/app/core/service/project.service';
@@ -20,17 +22,24 @@ export class ProjectChatComponent implements AfterViewInit, OnDestroy {
   activatedRoute = inject(ActivatedRoute)
   route = inject(Router)
 
-  projectId: string = ''
+  projectId: string = this.activatedRoute.parent?.snapshot.paramMap.get('projectId') ?? ''
   currentUser = this.currentUserService.currentUserSig()
 
-  @ViewChild('boxMessages', {static: true}) boxMessages!: ElementRef
-
+  @ViewChild('boxMessages', { static: true }) boxMessages!: ElementRef
+  
   messageControl: FormControl = new FormControl()
+  messagesList$?: Observable<MessageResponse[]>
 
   ngAfterViewInit(): void {
-    const projectId: string = this.activatedRoute.parent?.snapshot.paramMap.get('projectId') ?? ''
+    this.messagesList$ = this.projectService.getMessages(this.projectId)
     this.chatService.connect()
-    // this.projectService.getMessages(projectId).subscribe(console.log)
+    this.messageService.getRecentMessages(this.projectId, () => this.getMessages())
+    setTimeout(() => {
+    }, 500);
+  }
+
+  private getMessages(): void {
+    this.messagesList$ = this.projectService.getMessages(this.projectId)
   }
 
   ngOnDestroy(): void {
@@ -38,213 +47,14 @@ export class ProjectChatComponent implements AfterViewInit, OnDestroy {
   }
 
   isFirst(index: number): boolean {
-    return this.messagesList[index].sender.userId !== this.messagesList[index--].sender.userId
+    //return this.messagesList[index].sender.userId !== this.messagesList[index--].sender.userId
+    return false
   }
 
   send() {
     this.chatService.send(this.projectId, this.messageControl.value)
     this.messageControl.reset()
+    this.getMessages()
   }
-
-  currentUserId: string = 'd82d261b-1f69-4e74-b995-4e8f6ef39573'
-
-  messagesList: Message[] = [
-    {
-      id: '1',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Hi! How are you?',
-      sendIn: '2024-06-15T09:00:00Z'
-    },
-    {
-      id: '3',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Are you doing anything interesting today?',
-      sendIn: '2024-06-15T09:02:00Z'
-    },
-    {
-      id: '2',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Hello! I\'m good, thanks. How about you?',
-      sendIn: '2024-06-15T09:01:00Z'
-    },
-    {
-      id: '4',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Not much, just relaxing. What about you?',
-      sendIn: '2024-06-15T09:03:00Z'
-    },
-    {
-      id: '5',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Do you have any plans for the weekend?',
-      sendIn: '2024-06-15T09:04:00Z'
-    },
-    {
-      id: '6',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Not yet. Maybe I\'ll hang out with some friends. And you?',
-      sendIn: '2024-06-15T09:05:00Z'
-    },
-    {
-      id: '7',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Sounds good. I\'ll probably spend some time with family.',
-      sendIn: '2024-06-15T09:06:00Z'
-    },
-    {
-      id: '8',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'That sounds nice. Family time is important.',
-      sendIn: '2024-06-15T09:07:00Z'
-    },
-    {
-      id: '9',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Yes, definitely. Let\'s catch up later.',
-      sendIn: '2024-06-15T09:08:00Z'
-    },
-    {
-      id: '10',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Sure thing. Talk to you later!',
-      sendIn: '2024-06-15T09:09:00Z'
-    },
-    {
-      id: '14',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Goodbye!',
-      sendIn: '2024-06-15T09:13:00Z'
-    },
-    {
-      id: '13',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Bye for now!',
-      sendIn: '2024-06-15T09:12:00Z'
-    },
-    {
-      id: '11',
-      sender: {
-        userId: this.currentUserId,
-        nickname: 'Senha',
-        avatarUrl: ''
-        },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Have a great day!',
-      sendIn: '2024-06-15T09:10:00Z'
-    },
-    {
-      id: '12',
-      sender: {
-        userId: 'other-user-id',
-        nickname: 'UserB',
-        avatarUrl: ''
-      },
-      project: {
-        id: '',
-        name: ''
-      },
-      content: 'Thanks, you too!',
-      sendIn: '2024-06-15T09:11:00Z'
-    },
-  ];
 
 }
